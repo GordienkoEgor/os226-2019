920d840b06ef28d438486e117a491fee2b21dd93
Egor Gordienko <egor.gordienko.00@gmail.com>
2019-09-08 21:39:54 +0300

    First homework added

--- example/main.sh
+++ example/main.sh
@@ -1,2 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
+most_common_value=0
+most_common_value_index=0	
+
+while read value
+do
+	let freq_arr[value]++
+	if (( $most_common_value < ${freq_arr[value]} )) 
+	then
+		most_common_value=${freq_arr[value]}
+		most_common_value_index=$value
+	fi
+done
+
+echo $most_common_value
