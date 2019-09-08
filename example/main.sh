a430bc8796e6d1218861037ada611da83f2a9f30
GordienkoEgor <43183357+GordienkoEgor@users.noreply.github.com>
2019-09-08 21:49:05 +0300

    Update main.sh

--- example/main.sh
+++ example/main.sh
@@ -6,10 +6,10 @@ most_common_value_index=0
 while read value
 do
 	let freq_arr[value]++
-	if (( $most_common_value < ${freq_arr[value]} )) 
+	if (( $most_common_value_index < ${freq_arr[value]} )) 
 	then
-		most_common_value=${freq_arr[value]}
-		most_common_value_index=$value
+		most_common_value_index=${freq_arr[value]}
+		most_common_value=$value
 	fi
 done
 
